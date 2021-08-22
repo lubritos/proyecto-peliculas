8bbb65550f9b83af67a637e5ee8514cd97d87432
Luciana Britos <luciana.dalma.britos@gmail.com>
2021-08-21 23:44:06 -0300

    modificando estilos

--- js/carrito.js
+++ js/carrito.js
@@ -11,7 +11,15 @@ function actualizarCarrito() {
     const divCarrito = document.getElementsByClassName('carrito')[0];
     let html2 = '';
     for (let i = 0; i < carrito.length; i++) {
-        html2 +=  `<div><h5>${carrito[i].nombre}</h5></div>`;
+        html2 +=  `<div class="d-flex m-2 p-2 w-100 h-100 align-items-center justify-content-center">
+                        <img src="${carrito[i].portada}" class="rounded mx-auto d-block" alt="" width="60">
+                        <div class="d-flex flex-column m-3">
+                            <h5>${carrito[i].nombre}</h5>
+                            <span>${carrito[i].precio}</span>
+                            <span>${carrito[i].genero}</span>
+                        </div>
+                    </div>
+                    `;
     }
     divCarrito.innerHTML = html2;
 }
